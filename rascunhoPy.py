e1470ab52ec6c13a5351b751c86bc23dbb8216da
Daniel Luz <daniel_almeida_luz@hotmail.com>
2015-06-13 08:13:55 -0300

    rascunho ideia

---
+++ rascunhoPy.py
@@ -0,0 +1,14 @@
+construcao de dados
+	le xml
+		monta arvore
+	monta objeto de dados com a arvore
+		identifica as tags que se deseja
+
+persistencia
+	abre conexao com o banco
+	salva dados
+
+#Como mostrar os dados?
+opcao de pesquisa de dados?
+interface?
+	
