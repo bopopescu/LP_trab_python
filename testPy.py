34b62d68f33e5213a15b71cad24b4d74e2ad86bb
danielluz31 <daniel_almeida_luz@hotmail.com>
2015-06-16 09:36:23 -0300

    Entao chupa se quiser

--- testPy.py
+++ testPy.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-import sys
+from os import listdir
 import xml.etree.ElementTree as ET
 import MySQLdb
 import threading
@@ -21,7 +21,8 @@ class QueueConsumer(threading.Thread):
 	def run(self):
 		while not self.shutdown:
 			if MessageQueue.mainQueue:
-				print MessageQueue.mainQueue
+				#print MessageQueue.mainQueue
+				print 'Consuming queue...'
 				self.ConsumeObject(MessageQueue.pop())
 			time.sleep(1)
 
@@ -35,6 +36,17 @@ class QueueConsumer(threading.Thread):
 		if label == 'DADOS-NUMERICOS':
 			return 'dadosNumericos'
 
+class QueueProvider(threading.Thread):
+	def __init__(self, xmlFile):
+		threading.Thread.__init__(self)
+		self.xmlFile = xmlFile
+	def run(self):
+		root = ET.parse(self.xmlFile).getroot()
+		mainData = filter(isDataOfInterest, root[0].attrib.items())
+		mainData.append(('NUMERO-IDENTIFICADOR', root.attrib['NUMERO-IDENTIFICADOR']))
+#		print mainData
+		MessageQueue.stack(mainData, root[0].tag)
+
 class DaoQueueConsumer:
 	"""Data responsible class"""
 	def openCon(void):
@@ -46,32 +58,35 @@ def insertIntoTable(label,data):
 		attList = [data['NUMERO-IDENTIFICADOR'], data['NOME-COMPLETO'], data['CIDADE-NASCIMENTO'], data['PERMISSAO-DE-DIVULGACAO']]
 		cursor.execute("""INSERT INTO dadosGerais VALUES (%s, %s, %s, %s)""", attList)
 		con.commit()
+	#if label=='dadosNumericos':
+
 def isDataOfInterest(data):
 	dataOfInterest = ['NOME-COMPLETO', 'PERMISSAO-DE-DIVULGACAO', 'CIDADE-NASCIMENTO', 'PAIS-DE-NASCIMENTO']
 	return data[0] in dataOfInterest
 
 #Main
-tree = ET.parse('curriculo.xml')
-root = tree.getroot()
+#tree = ET.parse('curriculo.xml')
+#root = tree.getroot()
 daoQueueConsumer = DaoQueueConsumer()
 con = daoQueueConsumer.openCon()
 cursor = con.cursor()
 qConsumer = QueueConsumer()
 try:
 	qConsumer.start()
-#thread.start_new_thread(qConsumer.ConsumeQueue(), ())
+	for xmlFile in listdir('xmlFiles'):
+		QueueProvider('xmlFiles/'+xmlFile).start()
 except:
 	print "Unable to start thread"
 print "Started..."
 
 #root[0].attrib.values() #valores do dict
-root[0].attrib.items() #lista de tuplas do tipo(key, value)
-someInterestingData = filter(isDataOfInterest, root[0].attrib.items())
-someInterestingData.append(('NUMERO-IDENTIFICADOR', root.attrib['NUMERO-IDENTIFICADOR']))
+#root[0].attrib.items() #lista de tuplas do tipo(key, value)
+#someInterestingData = filter(isDataOfInterest, root[0].attrib.items())
+#someInterestingData.append(('NUMERO-IDENTIFICADOR', root.attrib['NUMERO-IDENTIFICADOR']))
 
-MessageQueue.stack(someInterestingData, root[0].tag)
+#MessageQueue.stack(someInterestingData, root[0].tag)
 
-time.sleep(5)
+time.sleep(60)
 
 qConsumer.shutdown = True
 qConsumer.join()
